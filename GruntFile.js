52a24510641e1d5cf044155b9277084db134d9c7
mum-never-proud <abhuz@hotmail.com>
2020-02-23 15:17:15 +0530

    gruntify

---
+++ GruntFile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: './src/<%= pkg.name %>.js',
+        dest: './lib/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.registerTask('default', ['uglify']);
+}
